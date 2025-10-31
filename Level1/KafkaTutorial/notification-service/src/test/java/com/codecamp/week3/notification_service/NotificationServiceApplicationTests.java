901943a1c725e033110753b48df66792050a72de
Manish Jha <manishkj2204@gmail.com>
2025-10-31 22:55:57 +0530

    basic setup

---
+++ Level1/KafkaTutorial/notification-service/src/test/java/com/codecamp/week3/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecamp.week3.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
